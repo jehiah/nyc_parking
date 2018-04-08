2d0f0c96c434ed9834282bf011822902bcf7b374
Jehiah Czebotar <jehiah@gmail.com>
2018-04-08 14:37:44 -0400

    update run cmd

--- run.sh
+++ run.sh
@@ -18,6 +18,7 @@ fi
 YMD=$(date +%Y-%m-%d)
 YMD=2017-12-24
 YMD=2017-01-13
+YMD=2018-04-08
 
 if [ ! -e data/locations_$YMD.csv ]; then
     echo "downloading locations_$YMD.csv"
@@ -28,5 +29,15 @@ if [ ! -e data/signs_$YMD.csv ]; then
     curl --silent -o data/signs_$YMD.csv "http://a841-dotweb01.nyc.gov/datafeeds/ParkingReg/signs.CSV" || exit 1
 fi
 
+echo "running nyc_parking analysis"
 ./nyc_parking --signs=data/signs_$YMD.csv --locations=data/locations_$YMD.csv --sign_summary > data/sign_data_$YMD.json
-generate_report.py --current-file=data/sign_data_$YMD.json
+if [ "$?" != 0 ]; then
+    echo "error running ./nyc_parking"
+    exit 1
+fi
+echo "generating report"
+./generate_report.py --current-file=data/sign_data_$YMD.json
+if [ "$?" != 0 ]; then
+    echo "error running ./generate_report.py"
+    exit 1
+fi
